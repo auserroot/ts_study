ef32aa68001f7f3a332f9aaca5a03f26e03b023a
auserroot <fauserfzy@gmail.com>
2021-05-21 09:56:22 +0800

    build

---
+++ test.js
@@ -0,0 +1,5 @@
+function test(p) {
+    return "Hello, " + p;
+}
+var u = "ator";
+document.body.innerHTML = test(u);
---
+++ test.ts
@@ -0,0 +1,6 @@
+function test(p){
+  return "Hello, " +p
+}
+let u = "ator"
+
+document.body.innerHTML = test(u)
